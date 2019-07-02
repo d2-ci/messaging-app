73248ebf227320437a2f497f0108ac5921862be4
Hendrik de Graaf <hendrik@dhis2.org>
2019-07-02 09:09:19 +0000

    03964bf fix: call updateMessageConversations with correct parameter signature

---
+++ precache-manifest.991704b5673d0c6a57498d21db6dbb1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050352990bc2f48b512849be29d1242e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9b94e26680aafd36685",
+    "url": "./static/css/main.7fc54a28.chunk.css"
+  },
+  {
+    "revision": "b0a63001dc28449c1ed3",
+    "url": "./static/js/2.89f339b2.chunk.js"
+  },
+  {
+    "revision": "a9b94e26680aafd36685",
+    "url": "./static/js/main.836ed8a9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
