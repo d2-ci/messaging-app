add684e310d6c8c8799a1e519fb02c73f12b20b2
GitHub <noreply@github.com>
2019-08-28 12:08:33 +0000

    1fdb83d Merge pull request #35 from dhis2/DHIS2-7198-failure-to-assign-user-to-message

---
+++ precache-manifest.b5df88f676ad1f1d0264264a38181193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2621bac33019e6c9293fe1d5fe11788b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30d188267c6329579164",
+    "url": "./static/css/main.7fc54a28.chunk.css"
+  },
+  {
+    "revision": "b0a63001dc28449c1ed3",
+    "url": "./static/js/2.89f339b2.chunk.js"
+  },
+  {
+    "revision": "30d188267c6329579164",
+    "url": "./static/js/main.a3dd9160.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
