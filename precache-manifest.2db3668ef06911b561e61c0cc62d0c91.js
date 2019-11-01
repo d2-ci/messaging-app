c60f5bec9643318bfee7775f7c3e025382f9dc87
Viktor Varland <varl@vlv.io>
2019-11-01 14:21:29 +0000

    3705775 chore: upgrade headerbar to the new one

---
+++ precache-manifest.2db3668ef06911b561e61c0cc62d0c91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee5b9e0de9db39210d1986bf7bd35cb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1e48aca3fa44a7b1ae9",
+    "url": "./static/css/main.7fc54a28.chunk.css"
+  },
+  {
+    "revision": "5e0bc3be36bc694f54ee",
+    "url": "./static/js/2.cf6a29e4.chunk.js"
+  },
+  {
+    "revision": "a1e48aca3fa44a7b1ae9",
+    "url": "./static/js/main.b98982df.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
