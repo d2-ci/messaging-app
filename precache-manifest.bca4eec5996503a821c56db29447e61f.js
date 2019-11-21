a650bac49e7bbd3caea5585ce425e6a7f4ca8867
Viktor Varland <varl@vlv.io>
2019-11-21 15:02:20 +0000

    309f56c chore(deps): update i18n extraction

---
+++ precache-manifest.bca4eec5996503a821c56db29447e61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63b9d3e044bc33edce6c9a9d9f5d1e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f2ed3d715fbe906c545",
+    "url": "./static/css/main.7fc54a28.chunk.css"
+  },
+  {
+    "revision": "c4a39e496134d48da05f",
+    "url": "./static/js/2.124dd92b.chunk.js"
+  },
+  {
+    "revision": "7f2ed3d715fbe906c545",
+    "url": "./static/js/main.4c31f87e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
