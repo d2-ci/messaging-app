39ce91df65b72480763af102efaa83d169d6ae02
Viktor Varland <varl@vlv.io>
2019-11-25 12:28:32 +0000

    488ad09 chore(translations): sync Arabic translations from transifex (master)

---
+++ precache-manifest.936fd4dd6eb377b07ac3fa9afd4d82e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33755de6a51404fd2fc44bf3c43006d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8143007a8f8bf629dacf",
+    "url": "./static/css/main.7fc54a28.chunk.css"
+  },
+  {
+    "revision": "c4a39e496134d48da05f",
+    "url": "./static/js/2.124dd92b.chunk.js"
+  },
+  {
+    "revision": "8143007a8f8bf629dacf",
+    "url": "./static/js/main.38a2ec06.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
